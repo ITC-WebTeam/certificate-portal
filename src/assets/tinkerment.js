bd71bedfa9d8d90c97bc8df3816d3cb0080eabf5
subham-agr <agrsubham423@gmail.com>
2022-10-08 00:58:35 +0530

    Tinker fix

--- src/assets/tinkerment.js
+++ src/assets/tinkerment.js
@@ -1,465 +1,465 @@
 const tinker_ment = [
-    {
-      "name": "Sarvadnya Desai"
-    },
-    {
-      "name": "Shravya Suresh"
-    },
-    {
-      "name": "Prachit Gupta"
-    },
-    {
-      "name": "Ayush Avinash Tamgadge"
-    },
-    {
-      "name": "Shounak Das"
-    },
-    {
-      "name": "Abhishek Kedare"
-    },
-    {
-      "name": "Ajinkya Patil"
-    },
-    {
-      "name": "Srushti Bangde"
-    },
-    {
-      "name": "Alok kumar"
-    },
-    {
-      "name": "Anukool Vikram"
-    },
-    {
-      "name": "Saatwik Amrawat"
-    },
-    {
-      "name": "Faiz Hashim"
-    },
-    {
-      "name": "Vashisht Ega"
-    },
-    {
-      "name": "Vedika Rathi"
-    },
-    {
-      "name": "Mudke Nilkanth"
-    },
-    {
-      "name": "Supritha Eaga"
-    },
-    {
-      "name": "Sahil Dharod"
-    },
-    {
-      "name": "Ravi Sah"
-    },
-    {
-      "name": "Aadhitya A B"
-    },
-    {
-      "name": "Rohan Kalbag"
-    },
-    {
-      "name": "Sarvadnya Desai"
-    },
-    {
-      "name": "Rohit Mandhyan"
-    },
-    {
-      "name": "Pragyesh Bajpai"
-    },
-    {
-      "name": "Aditya Prasad"
-    },
-    {
-      "name": "Prabhash Kumar"
-    },
-    {
-      "name": "Mehul Agrawal"
-    },
-    {
-      "name": "Chaitanya Katti"
-    },
-    {
-      "name": "Shaikh Hassan"
-    },
-    {
-      "name": "Manideep Gumalapauram"
-    },
-    {
-      "name": "Hanish Dhanwalkar"
-    },
-    {
-      "name": "Saumya Sheth"
-    },
-    {
-      "name": "Ashutosh Mulchandani"
-    },
-    {
-      "name": "Jason Gomez"
-    },
-    {
-      "name": "Sandeep Rawat"
-    },
-    {
-      "name": "Akshat Goyal"
-    },
-    {
-      "name": "Ayush Agarwal"
-    },
-    {
-      "name": "Anshul Verma"
-    },
-    {
-      "name": "Kartik Khandelwal"
-    },
-    {
-      "name": "K Chakravorthy"
-    },
-    {
-      "name": "Deelip jani"
-    },
-    {
-      "name": "Gohil Ganeshbhai"
-    },
-    {
-      "name": "Vinu S"
-    },
-    {
-      "name": "Baggam Rakshan Tej"
-    },
-    {
-      "name": "Vaibhav Upadhyay"
-    },
-    {
-      "name": "Prachit Gupta"
-    },
-    {
-      "name": "Shobhanaa Kaushikee"
-    },
-    {
-      "name": "Sayali Patil"
-    },
-    {
-      "name": "Kartik Nair"
-    },
-    {
-      "name": "Siddhesh Chaudhari"
-    },
-    {
-      "name": "Saukhya Telge"
-    },
-    {
-      "name": "Akash Palaniswami"
-    },
-    {
-      "name": "Anish V"
-    },
-    {
-      "name": "Abhishek kushwah"
-    },
-    {
-      "name": "Kher Vasudevanand"
-    },
-    {
-      "name": "Pooja Shelar"
-    },
-    {
-      "name": "Vedagya Bhalotia"
-    },
-    {
-      "name": "Shravya S"
-    },
-    {
-      "name": "Simran Singh"
-    },
-    {
-      "name": "Kushal Agarwal"
-    },
-    {
-      "name": "Abhinav Vaishnav"
-    },
-    {
-      "name": "Chinta Madhav"
-    },
-    {
-      "name": "Ayush Tamgadge"
-    },
-    {
-      "name": "Shambharkar Prakash"
-    },
-    {
-      "name": "Gowtham S"
-    },
-    {
-      "name": "Harshraj Chaudhari"
-    },
-    {
-      "name": "Anshika gangwar"
-    },
-    {
-      "name": "Shraman Santara"
-    },
-    {
-      "name": "Kandregula Ruthvik"
-    },
-    {
-      "name": "Aaryan Sharma"
-    },
-    {
-      "name": "Sagar Kumar"
-    },
-    {
-      "name": "Pranjal Animesh"
-    },
-    {
-      "name": "Aryan Bhosale"
-    },
-    {
-      "name": "Dhruv Bhandari"
-    },
-    {
-      "name": "Parth Bansal"
-    },
-    {
-      "name": "Sidhanth Soundarrajan"
-    },
-    {
-      "name": "Anushika Poddar"
-    },
-    {
-      "name": "Vinay Sutar"
-    },
-    {
-      "name": "Vaibhav Vishal"
-    },
-    {
-      "name": "Resnick Singh"
-    },
-    {
-      "name": "Neeraja Patil"
-    },
-    {
-      "name": "Rahul B"
-    },
-    {
-      "name": "Harsh Chaurasia"
-    },
-    {
-      "name": "Eshu ."
-    },
-    {
-      "name": "Arjun R"
-    },
-    {
-      "name": "Bekate Dnyaneshwar"
-    },
-    {
-      "name": "Aarushi Agarwal"
-    },
-    {
-      "name": "Harshali Gawand"
-    },
-    {
-      "name": "Archit Gupta"
-    },
-    {
-      "name": "Dhruvi Ganatra"
-    },
-    {
-      "name": "Arnav Patel"
-    },
-    {
-      "name": "Prateek Tambia"
-    },
-    {
-      "name": "Jathin Sai Ganesh Angotu"
-    },
-    {
-      "name": "Kapil Kumbhare"
-    },
-    {
-      "name": "Satush Parikh"
-    },
-    {
-      "name": "Pritesh Sahu"
-    },
-    {
-      "name": "Aditya Raj"
-    },
-    {
-      "name": "Siddharth Stephen"
-    },
-    {
-      "name": "Sindhu Banoth"
-    },
-    {
-      "name": "Mohammad Hanzala Ansari"
-    },
-    {
-      "name": "Srishti Badaya"
-    },
-    {
-      "name": "Ananya Nawale"
-    },
-    {
-      "name": "Sajal Deolikar"
-    },
-    {
-      "name": "Aayushi Barve"
-    },
-    {
-      "name": "Shravani Kode"
-    },
-    {
-      "name": "Raj Kumar"
-    },
-    {
-      "name": "Ayush kale"
-    },
-    {
-      "name": "Kartik Chikkanagoudar"
-    },
-    {
-      "name": "Eshica Arya"
-    },
-    {
-      "name": "Harsheet Singh"
-    },
-    {
-      "name": "Soham Inamdar"
-    },
-    {
-      "name": "Tarun Jain"
-    },
-    {
-      "name": "Polamreddy Krishna"
-    },
-    {
-      "name": "Vaibhavi Shinde"
-    },
-    {
-      "name": "Subham Agrawal"
-    },
-    {
-      "name": "Moningi Srija"
-    },
-    {
-      "name": "Poojita Mishra"
-    },
-    {
-      "name": "Jaideep Kotani"
-    },
-    {
-      "name": "Tavva D N V S Krishna Kamal"
-    },
-    {
-      "name": "Chavali Krishna Kaushik"
-    },
-    {
-      "name": "Abhijit Kumar"
-    },
-    {
-      "name": "Ameer Hamzan"
-    },
-    {
-      "name": "Shounak Das"
-    },
-    {
-      "name": "Pranav Singla"
-    },
-    {
-      "name": "Deepika jandu"
-    },
-    {
-      "name": "Krutheeka K J"
-    },
-    {
-      "name": "Sawan Meena"
-    },
-    {
-      "name": "Annirudh P"
-    },
-    {
-      "name": "Aditya Vibhute"
-    },
-    {
-      "name": "Scaria Kochidanadu"
-    },
-    {
-      "name": "Atishay Jain"
-    },
-    {
-      "name": "Pratham Chandra"
-    },
-    {
-      "name": "Nayantara R"
-    },
-    {
-      "name": "Ojas patil"
-    },
-    {
-      "name": "Thakre Chandrashekhar"
-    },
-    {
-      "name": "Alfia Khan"
-    },
-    {
-      "name": "Shivansh Gupta"
-    },
-    {
-      "name": "Ashutosh Bharti"
-    },
-    {
-      "name": "Samar Agarwal"
-    },
-    {
-      "name": "Kavan Vavadiya"
-    },
-    {
-      "name": "Anand Arun"
-    },
-    {
-      "name": "Abhishek Sarda"
-    },
-    {
-      "name": "Sahil Zodge"
-    },
-    {
-      "name": "Abhishek Kedare"
-    },
-    {
-      "name": "Tarang Saxena"
-    },
-    {
-      "name": "Dhruv Khetan"
-    },
-    {
-      "name": "Krishna Baldwa"
-    },
-    {
-      "name": "Shreyansh Chandra"
-    },
-    {
-      "name": "Anway Deshpande"
-    },
-    {
-      "name": "Arushi Bansal"
-    },
-    {
-      "name": "Nayanshree Sonowal"
-    },
-    {
-      "name": "Abhijeet  Jha"
-    },
-    {
-      "name": "Asita Dutta Sharma"
-    },
-    {
-      "name": "Tanmay  Jain"
-    },
-    {
-      "name": "Dewaansh  Vijayvargiya"
-    }
-  ]
+  {
+    "name": "Aadhitya A B"
+  },
+  {
+    "name": "Aarushi Agarwal"
+  },
+  {
+    "name": "Aaryan Sharma"
+  },
+  {
+    "name": "Aayushi Barve"
+  },
+  {
+    "name": "Abhijeet  Jha"
+  },
+  {
+    "name": "Abhijit Kumar"
+  },
+  {
+    "name": "Abhinav Vaishnav"
+  },
+  {
+    "name": "Abhishek Kedare"
+  },
+  {
+    "name": "Abhishek Kedare"
+  },
+  {
+    "name": "Abhishek kushwah"
+  },
+  {
+    "name": "Abhishek Sarda"
+  },
+  {
+    "name": "Aditya Prasad"
+  },
+  {
+    "name": "Aditya Raj"
+  },
+  {
+    "name": "Aditya Vibhute"
+  },
+  {
+    "name": "Ajinkya Patil"
+  },
+  {
+    "name": "Akash Palaniswami"
+  },
+  {
+    "name": "Akshat Goyal"
+  },
+  {
+    "name": "Alfia Khan"
+  },
+  {
+    "name": "Alok kumar"
+  },
+  {
+    "name": "Ameer Hamzan"
+  },
+  {
+    "name": "Anand Arun"
+  },
+  {
+    "name": "Ananya Nawale"
+  },
+  {
+    "name": "Anish V"
+  },
+  {
+    "name": "Annirudh P"
+  },
+  {
+    "name": "Anshika gangwar"
+  },
+  {
+    "name": "Anshul Verma"
+  },
+  {
+    "name": "Anukool Vikram"
+  },
+  {
+    "name": "Anushika Poddar"
+  },
+  {
+    "name": "Anway Deshpande"
+  },
+  {
+    "name": "Archit Gupta"
+  },
+  {
+    "name": "Arjun R"
+  },
+  {
+    "name": "Arnav Patel"
+  },
+  {
+    "name": "Arushi Bansal"
+  },
+  {
+    "name": "Aryan Bhosale"
+  },
+  {
+    "name": "Ashutosh Bharti"
+  },
+  {
+    "name": "Ashutosh Mulchandani"
+  },
+  {
+    "name": "Asita  Dutta Sharma"
+  },
+  {
+    "name": "Atishay Jain"
+  },
+  {
+    "name": "Ayush Agarwal"
+  },
+  {
+    "name": "Ayush Avinash Tamgadge"
+  },
+  {
+    "name": "Ayush kale"
+  },
+  {
+    "name": "Ayush Tamgadge"
+  },
+  {
+    "name": "Baggam Rakshan Tej"
+  },
+  {
+    "name": "Bekate Dnyaneshwar"
+  },
+  {
+    "name": "Chaitanya Katti"
+  },
+  {
+    "name": "Chavali Krishna Kaushik"
+  },
+  {
+    "name": "Chinta Madhav"
+  },
+  {
+    "name": "Deelip jani"
+  },
+  {
+    "name": "Deepika jandu"
+  },
+  {
+    "name": "Dewaansh  Vijayvargiya"
+  },
+  {
+    "name": "Dhruv Bhandari"
+  },
+  {
+    "name": "Dhruv Khetan"
+  },
+  {
+    "name": "Dhruvi Ganatra"
+  },
+  {
+    "name": "Eshica Arya"
+  },
+  {
+    "name": "Eshu ."
+  },
+  {
+    "name": "Faiz Hashim"
+  },
+  {
+    "name": "Gohil Ganeshbhai"
+  },
+  {
+    "name": "Gowtham S"
+  },
+  {
+    "name": "Hanish Dhanwalkar"
+  },
+  {
+    "name": "Harsh Chaurasia"
+  },
+  {
+    "name": "Harshali Gawand"
+  },
+  {
+    "name": "Harsheet Singh"
+  },
+  {
+    "name": "Harshraj Chaudhari"
+  },
+  {
+    "name": "Jaideep Kotani"
+  },
+  {
+    "name": "Jason Gomez"
+  },
+  {
+    "name": "Jathin Sai Ganesh Angotu"
+  },
+  {
+    "name": "K Chakravorthy"
+  },
+  {
+    "name": "Kandregula Ruthvik"
+  },
+  {
+    "name": "Kapil Kumbhare"
+  },
+  {
+    "name": "Kartik Chikkanagoudar"
+  },
+  {
+    "name": "Kartik Khandelwal"
+  },
+  {
+    "name": "Kartik Nair"
+  },
+  {
+    "name": "Kavan Vavadiya"
+  },
+  {
+    "name": "Kher Vasudevanand"
+  },
+  {
+    "name": "Krishna Baldwa"
+  },
+  {
+    "name": "Krutheeka K J"
+  },
+  {
+    "name": "Kushal Agarwal"
+  },
+  {
+    "name": "Manideep Gumalapauram"
+  },
+  {
+    "name": "Mehul Agrawal"
+  },
+  {
+    "name": "Mohammad Hanzala Ansari"
+  },
+  {
+    "name": "Moningi Srija"
+  },
+  {
+    "name": "Mudke Nilkanth"
+  },
+  {
+    "name": "Nayanshree Sonowal"
+  },
+  {
+    "name": "Nayantara R"
+  },
+  {
+    "name": "Neeraja Patil"
+  },
+  {
+    "name": "Ojas patil"
+  },
+  {
+    "name": "Parth Bansal"
+  },
+  {
+    "name": "Polamreddy Krishna"
+  },
+  {
+    "name": "Pooja Shelar"
+  },
+  {
+    "name": "Poojita Mishra"
+  },
+  {
+    "name": "Prabhash Kumar"
+  },
+  {
+    "name": "Prachit Gupta"
+  },
+  {
+    "name": "Prachit Gupta"
+  },
+  {
+    "name": "Pragyesh Bajpai"
+  },
+  {
+    "name": "Pranav Singla"
+  },
+  {
+    "name": "Pranjal Animesh"
+  },
+  {
+    "name": "Prateek Tambia"
+  },
+  {
+    "name": "Pratham Chandra"
+  },
+  {
+    "name": "Pritesh Sahu"
+  },
+  {
+    "name": "Rahul B"
+  },
+  {
+    "name": "Raj Kumar"
+  },
+  {
+    "name": "Ravi Sah"
+  },
+  {
+    "name": "Resnick Singh"
+  },
+  {
+    "name": "Rohan Kalbag"
+  },
+  {
+    "name": "Rohit Mandhyan"
+  },
+  {
+    "name": "Saatwik Amrawat"
+  },
+  {
+    "name": "Sagar Kumar"
+  },
+  {
+    "name": "Sahil Dharod"
+  },
+  {
+    "name": "Sahil Zodge"
+  },
+  {
+    "name": "Sajal Deolikar"
+  },
+  {
+    "name": "Samar Agarwal"
+  },
+  {
+    "name": "Sandeep Rawat"
+  },
+  {
+    "name": "Sarvadnya Desai"
+  },
+  {
+    "name": "Sarvadnya Desai"
+  },
+  {
+    "name": "Satush Parikh"
+  },
+  {
+    "name": "Saukhya Telge"
+  },
+  {
+    "name": "Saumya Sheth"
+  },
+  {
+    "name": "Sawan Meena"
+  },
+  {
+    "name": "Sayali Patil"
+  },
+  {
+    "name": "Scaria Kochidanadu"
+  },
+  {
+    "name": "Shaikh Hassan"
+  },
+  {
+    "name": "Shambharkar Prakash"
+  },
+  {
+    "name": "Shivansh Gupta"
+  },
+  {
+    "name": "Shobhanaa Kaushikee"
+  },
+  {
+    "name": "Shounak Das"
+  },
+  {
+    "name": "Shounak Das"
+  },
+  {
+    "name": "Shraman Santara"
+  },
+  {
+    "name": "Shravani Kode"
+  },
+  {
+    "name": "Shravya S"
+  },
+  {
+    "name": "Shravya Suresh"
+  },
+  {
+    "name": "Shreyansh Chandra"
+  },
+  {
+    "name": "Siddharth Stephen"
+  },
+  {
+    "name": "Siddhesh Chaudhari"
+  },
+  {
+    "name": "Sidhanth Soundarrajan"
+  },
+  {
+    "name": "Simran Singh"
+  },
+  {
+    "name": "Sindhu Banoth"
+  },
+  {
+    "name": "Soham Inamdar"
+  },
+  {
+    "name": "Srishti Badaya"
+  },
+  {
+    "name": "Srushti Bangde"
+  },
+  {
+    "name": "Subham Agrawal"
+  },
+  {
+    "name": "Supritha Eaga"
+  },
+  {
+    "name": "Tanmay  Jain"
+  },
+  {
+    "name": "Tarang Saxena"
+  },
+  {
+    "name": "Tarun Jain"
+  },
+  {
+    "name": "Tavva D N V S Krishna Kamal"
+  },
+  {
+    "name": "Thakre Chandrashekhar"
+  },
+  {
+    "name": "Vaibhav Upadhyay"
+  },
+  {
+    "name": "Vaibhav Vishal"
+  },
+  {
+    "name": "Vaibhavi Shinde"
+  },
+  {
+    "name": "Vashisht Ega"
+  },
+  {
+    "name": "Vedagya Bhalotia"
+  },
+  {
+    "name": "Vedika Rathi"
+  },
+  {
+    "name": "Vinay Sutar"
+  },
+  {
+    "name": "Vinu S"
+  }
+]
   export{ tinker_ment}
